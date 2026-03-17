7ce4757c1da89bad5ff50374feb18d1cf76206ac
assissantosvitor <assis.santos.vitor@escola.pr.gov.br>
2026-03-17 09:38:50 -0300

    EXU

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.getElementById('form-cadastro').addEventListener('submit', function(e) 
     // Mensagem solicitada
     alert("roubei sua informaçoes");
 
-    // Redireciona para a página da "Nike"
+    // Redireciona para a página de sucesso (Estilo Nike)
     window.location.href = "sucesso.html";
 });
